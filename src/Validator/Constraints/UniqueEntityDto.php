ef1e27f5c5958d21745eeedfdc03bd53552e5d3e
Ronald Marfoldi <marforon@gmail.com>
2023-02-07 15:41:13 +0100

    Add Unique validators

---
+++ src/Validator/Constraints/UniqueEntityDto.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AnzuSystems\CommonBundle\Validator\Constraints;
+
+use AnzuSystems\CommonBundle\Exception\ValidationException;
+use Attribute;
+use Symfony\Component\Validator\Constraint;
+
+#[Attribute(Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE)]
+final class UniqueEntityDto extends Constraint
+{
+    public string $message = ValidationException::ERROR_FIELD_UNIQUE;
+
+    public function __construct(
+        /** @var class-string */
+        public readonly string $entity,
+        /** @var non-empty-list<string> */
+        public readonly array $fields,
+        /** @var list<string> */
+        public readonly array $errorAtPath = [],
+        mixed $options = null,
+        array $groups = null,
+        mixed $payload = null,
+    ) {
+        parent::__construct($options, $groups, $payload);
+    }
+
+    public function getTargets(): string
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+}
