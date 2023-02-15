9f4aa00a98bbcbfe27c4e526b6899f911f2e94a4
Ronald Marfoldi <marforon@gmail.com>
2023-02-15 14:03:19 +0100

    Rename create user data delete job controller action and allow string for an EntityExists validator

--- src/Validator/Constraints/EntityExistsValidator.php
+++ src/Validator/Constraints/EntityExistsValidator.php
@@ -8,6 +8,7 @@ use AnzuSystems\CommonBundle\Repository\AnzuRepositoryInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 
 final class EntityExistsValidator extends ConstraintValidator
 {
@@ -25,9 +26,13 @@ final class EntityExistsValidator extends ConstraintValidator
             return;
         }
 
+        if (false === is_int($value) && false === is_string($value)) {
+            throw new UnexpectedTypeException($value, 'string|integer');
+        }
+
         /** @var AnzuRepositoryInterface $repo */
         $repo = $this->entityManager->getRepository($constraint->entity);
-        if ($repo->exists((int) $value)) {
+        if ($repo->exists(is_numeric($value) ? (int) $value : $value)) {
             return;
         }
 
