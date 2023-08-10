8482a33fc9c4299258796a30069fb915d65866fb
Ronald Marfoldi <marforon@gmail.com>
2023-08-10 13:17:05 +0200

    Fix app name variable

--- src/Tests/Traits/AnzuKernelTrait.php
+++ src/Tests/Traits/AnzuKernelTrait.php
@@ -18,7 +18,7 @@ trait AnzuKernelTrait
 
         return new $kernelClass(
             appNamespace: (string) self::resolveKernelOption($options, 'namespace', 'APP_NAMESPACE', ''),
-            appSystem: (string) self::resolveKernelOption($options, 'system', 'APP_SYSTEM', ''),
+            appSystem: (string) self::resolveKernelOption($options, 'name', 'APP_NAME', ''),
             appVersion: (string) self::resolveKernelOption($options, 'version', 'APP_VERSION', '0.0.0'),
             appReadOnlyMode: (bool) self::resolveKernelOption($options, 'debug', 'APP_READ_ONLY_MODE', false),
             environment: (string) self::resolveKernelOption($options, 'environment', 'APP_ENV', 'test'),
