83dceca5602dd68e49919f416c63901a04c1bdc8
Tomas Hermanek <tomas.hermanek@petitpress.sk>
2024-05-29 10:27:15 +0200

    Removed dump

--- src/Event/Listener/ExceptionListener.php
+++ src/Event/Listener/ExceptionListener.php
@@ -34,9 +34,6 @@ final class ExceptionListener
     public function __invoke(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
-
-        dump($exception);
-
         $handler = $this->getHandler($exception);
 
         if ($this->isRequestMatchForAllowedHandling($event->getRequest())) {
